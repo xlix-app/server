e4095c7e9bf63b9816f50c20889db53e3124f71e
FssAy <49795742+FssAy@users.noreply.github.com>
2024-05-05 17:12:11 +0200

    Add `Sessionless` context object

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -7,5 +7,10 @@ pub mod time;
 
 pub use as_res::*;
 use std::borrow::Cow;
+use sessionless::Sessionless;
 
 pub type AnyString = Cow<'static, str>;
+
+lazy_static! {
+    pub static ref SESSIONLESS: Sessionless = Sessionless::new();
+}
